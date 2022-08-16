74100b450517db7029b70446817ac76545a67ae5
RamaOnate <ramadelarbolito@gmail.com>
2022-08-16 16:47:54 -0300

    Initial work for adding pokemon sprites

--- src/components/PokemonPicture.tsx
+++ src/components/PokemonPicture.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react"
+import RandomPokemonGif from "./Pokedex"
+import { useState } from "react"
+
+
+
 const PokemonPicture = () => {
+
+    const [pokemon, setPokemon] = useState(RandomPokemonGif())
+
+    useEffect((): void => {
+        handleNextPokemon()
+        console.log(pokemon)
+    }, [])
+
+    function handleNextPokemon(): void {
+        setPokemon(RandomPokemonGif())
+        console.log(pokemon)
+    }
+
     return (
         <div className="pokemon-picture-container border">
-            <div className="black pokemon-picture" />
+            <div className="black pokemon-picture" style={{ backgroundImage: `url('${pokemon.image}')` }} onClick={handleNextPokemon} />
         </div>
     )
 }
